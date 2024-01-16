982b92d45f0275c1618be8b387f6d09aa3442249
Jatin Chauhan <jatinchauhan615@gmail.com>
2024-01-16 14:32:19 +0000

    update

--- Strings/reverseString.cpp
+++ Strings/reverseString.cpp
@@ -8,15 +8,21 @@ int main(){
     cout << "Enter the string : ";
     cin >> str;
 
+    // 'i' 0th index ko point karega aur j ko us index par point karayenge jaha last character hai string ka
+
     int i = 0, j = 0;
+
+    // this loop finds the index of null character
+    
     while(str[j] != '\0'){
         j++;
     }
-    j--;
+    j--;                // last character ka index null character ke index se ek kam hoga
+
+    // fir normal reverse karne ke liye loop similar to that in integer arrays
 
     while(i<j){
-        swap(str[i], str[j]);
-        i++; j--;
+        swap(str[i++], str[j--]);
     }
 
     cout << "Reversed String is : " << str << endl;
